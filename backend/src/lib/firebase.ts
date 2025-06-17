97d1a3bfd0476bf0376a7e583a8fc24387db7b7f
Pau Larrea <paularrea@Mac.home>
2025-06-17 17:09:31 +0200

    Use FIREBASE_KEY_BASE64 for Firebase admin credentials

--- backend/src/lib/firebase.ts
+++ backend/src/lib/firebase.ts
@@ -12,3 +12,25 @@ if (!admin.apps.length) {
 
 export const bucket = admin.storage().bucket();
 export default admin;
+
+// import admin from 'firebase-admin';
+
+// const serviceAccountBase64 = process.env.FIREBASE_KEY_BASE64;
+
+// if (!serviceAccountBase64) {
+//   throw new Error('FIREBASE_KEY_BASE64 env variable is not set.');
+// }
+
+// const serviceAccount = JSON.parse(
+//   Buffer.from(serviceAccountBase64, 'base64').toString('utf-8')
+// );
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   });
+// }
+
+// export const bucket = admin.storage().bucket();
+// export default admin;
