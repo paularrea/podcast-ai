4a344d648451277b24a63e5d0cd24921f265355d
Pau Larrea <paularrea@Mac.home>
2025-06-17 19:04:08 +0200

    fix: fully switch to base64 firebase key

--- backend/src/lib/firebase.ts
+++ backend/src/lib/firebase.ts
@@ -1,29 +1,7 @@
-import admin from 'firebase-admin';
-import path from 'path';
-
-const serviceAccount = require(path.resolve(__dirname, '../../firebase-key.json'));
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  });
-}
-
-export const bucket = admin.storage().bucket();
-export default admin;
-
 // import admin from 'firebase-admin';
+// import path from 'path';
 
-// const serviceAccountBase64 = process.env.FIREBASE_KEY_BASE64;
-
-// if (!serviceAccountBase64) {
-//   throw new Error('FIREBASE_KEY_BASE64 env variable is not set.');
-// }
-
-// const serviceAccount = JSON.parse(
-//   Buffer.from(serviceAccountBase64, 'base64').toString('utf-8')
-// );
+// const serviceAccount = require(path.resolve(__dirname, '../../firebase-key.json'));
 
 // if (!admin.apps.length) {
 //   admin.initializeApp({
@@ -34,3 +12,25 @@ export default admin;
 
 // export const bucket = admin.storage().bucket();
 // export default admin;
+
+import admin from 'firebase-admin';
+
+const serviceAccountBase64 = process.env.FIREBASE_KEY_BASE64;
+
+if (!serviceAccountBase64) {
+  throw new Error('FIREBASE_KEY_BASE64 env variable is not set.');
+}
+
+const serviceAccount = JSON.parse(
+  Buffer.from(serviceAccountBase64, 'base64').toString('utf-8')
+);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  });
+}
+
+export const bucket = admin.storage().bucket();
+export default admin;
