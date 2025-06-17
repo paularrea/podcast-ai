1c295bef2fce7176127ffd518be255bd2c6af1f9
Pau Larrea <paularrea@Mac.home>
2025-06-17 23:05:36 +0200

    appdelegate.mm

--- ios/PodcastAI/AppDelegate.mm
+++ ios/PodcastAI/AppDelegate.mm
@@ -1,58 +1,47 @@
-#import <UIKit/UIKit.h>
-#import <Expo/Expo.h>
-#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
+#import <React/RCTRootView.h>
+#import <Expo/Expo.h>
+#import <ReactAppDependencyProvider/ReactAppDependencyProvider.h>
 
-@interface ReactNativeDelegate : NSObject <ExpoReactNativeFactoryDelegate>
-@end
+@interface AppDelegate ()
 
-@implementation ReactNativeDelegate
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
-  return bridge.bundleURL ?: [self bundleURL];
-}
-
-- (NSURL *)bundleURL {
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
-
-@end
-
-@interface AppDelegate : ExpoAppDelegate
 @property (nonatomic, strong) UIWindow *window;
 @property (nonatomic, strong) ExpoReactNativeFactory *reactNativeFactory;
 @property (nonatomic, strong) ReactNativeDelegate *reactNativeDelegate;
+
 @end
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  self.reactNativeDelegate = [ReactNativeDelegate new];
-  self.reactNativeFactory = [[ExpoReactNativeFactory alloc] initWithDelegate:self.reactNativeDelegate];
-  [self.reactNativeDelegate setDependencyProvider:[RCTAppDependencyProvider new]];
-  [self bindReactNativeFactory:self.reactNativeFactory];
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  ReactNativeDelegate *delegate = [[ReactNativeDelegate alloc] init];
+  ExpoReactNativeFactory *factory = [[ExpoReactNativeFactory alloc] initWithDelegate:delegate];
+  delegate.dependencyProvider = [RCTAppDependencyProvider new];
+
+  self.reactNativeFactory = factory;
+  self.reactNativeDelegate = delegate;
+
+  [self bindReactNativeFactory:factory];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  [self.reactNativeFactory startReactNativeWithModuleName:@"main"
-                                                       inWindow:self.window
-                                                 launchOptions:launchOptions];
+  [factory startReactNativeWithModuleName:@"main"
+                           reactDelegate:delegate
+                                 inWindow:self.window
+                           launchOptions:launchOptions];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+// URL scheme handling
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-  return [RCTLinkingManager application:app openURL:url options:options] ||
-         [super application:app openURL:url options:options];
+  return [RCTLinkingManager application:app openURL:url options:options] || [super application:app openURL:url options:options];
 }
 
-- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler {
-  BOOL result = [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-  return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || result;
+// Universal links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
+  return [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
 }
 
 @end
